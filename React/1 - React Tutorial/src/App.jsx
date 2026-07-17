42ae8fd6d3c83a2bce8a8f851dd62391864149e7
Nattawat Lertchidumrongsri <NattawatLert0747@gmail.com>
2026-07-17 12:58:17 +0700

    feat: add React Tutorial

---
+++ React/1 - React Tutorial/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Food from "./Food";
+
+function App() {
+  return (
+      <>
+         <Footer/>
+         <Header />
+         <Food/>
+         <Food/>
+      </>
+  );       
+}
+
+export default App;
