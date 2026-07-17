da4ff6e48b9d9baa3992cd95d5bc0be0af3d69d0
Nattawat Lertchidumrongsri <NattawatLert0747@gmail.com>
2026-07-17 22:58:13 +0700

    feat: Update OBJECTS in state

---
+++ React/A12 - Update OBJECTS in state/src/MyComponent.jsx
@@ -0,0 +1,32 @@
+
+import React, {useState} from 'react';
+
+function MyComponent(){
+
+    const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
+
+    function handleYearChange(event){
+        setCar(c => ({...c, year: event.target.value}));
+    }
+
+    function handleMakeChange(){
+        setCar(c => ({...c, make: event.target.value}));
+    }
+
+    function handleModelChange(){
+        setCar(c => ({...car, model: event.target.value}));
+    }
+
+    return(
+        <>
+        <div>
+            <p>Your favorite car is : {car.year} {car.make} {car.model}</p>
+
+            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
+            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
+            <input type="text" value={car.model} onChange={handleModelChange}/><br/>
+        </div>
+        </>);
+}
+
+export default MyComponent
